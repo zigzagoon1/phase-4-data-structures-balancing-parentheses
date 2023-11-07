442cd51796d6929231523bb36f06291296d4ea73
KellyMark <kmark602@gmail.com>
2023-11-07 17:19:54 -0500

    commit tree 43d4ce79f9c2c6e5bb762d6045442c7f79c01bc2

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,21 @@
 require_relative './stack'
 
 # your code here
+    def balancing_parentheses(string)
+        stack = Stack.new
+        string.each_char do |char|
+            if char == '('
+                stack.push(char)
+            elsif char == ')'
+                if stack.peek == '('
+                    stack.pop
+                else
+                    stack.push(char)
+                end
+            else
+                puts 'Character not parentheses'
+            end
+        end
+
+        stack.size
+    end
